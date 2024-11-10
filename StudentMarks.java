774b7690fc1ac41c5f6741094fbd5118e3ed65c8
FerdiRoth <FerdiRoth@users.noreply.github.com>
2024-11-10 11:57:51 +1000

    highest & lowest mark

--- StudentMarks.java
+++ StudentMarks.java
@@ -12,36 +12,50 @@ public class StudentMarks
       
       int StudentCount = 5;
       float[] marks = new float[StudentCount];
-      Scanner scan = new Scanner(System.in);
       
+      // Input student grades
+      Scanner scan = new Scanner(System.in);
       System.out.println("Give 30 Student Marks");
       
       for (int i = 0; i < StudentCount; i++){
+          // While loop to leave if input is correct
           while (true){
-        
-        
+              
           System.out.printf("Student %d Mark: ", i+1);
           
           float temp = scan.nextFloat();
           
           if (temp < 0 || temp > 30){
-              
-              System.out.println("Invalid marks, please reenter");
+              System.out.println("Invalid mark, please reenter");
               continue;
           } else {
               marks[i] = temp;
               break;
           }
-          
-          
         }
       }
-      
+      // Stored marks array output
       System.out.println("Student Marks put in:");
       for (int i = 0; i < StudentCount; i++){
           System.out.printf("Student %d: ", i+1, marks[i]);
           System.out.println(marks[i]);
-         
       }
+      
+      //Compute highest and lowest marks
+      //Initialising highest and lowest marks with first mark
+      float highestMark = marks[0];
+      float lowestMark = marks[0];
+      //loop through marks to find highest and lowest
+      for (int i = 1; i < StudentCount; i++){
+          if(marks[i] > highestMark){
+              highestMark = marks[i];
+          }
+          if(marks[i] < lowestMark){
+              lowestMark = marks[i];
+          }
+      }
+      //Output highest and lowest Mark
+      System.out.printf("Highest mark: %.1f\n", highestMark);
+      System.out.printf("Lowest mark: %.1f\n ", lowestMark);
   }
 }
