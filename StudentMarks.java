2ac88a9b4b52d18927e4cfed7bab6cbef36bcaf8
FerdiRoth <FerdiRoth@users.noreply.github.com>
2024-11-10 10:30:11 +1000

    user Input

--- StudentMarks.java
+++ StudentMarks.java
@@ -5,29 +5,14 @@
  * @author (your name)
  * @version (a version number or a date)
  */
+import java.util.Scanner;
 public class StudentMarks
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class StudentMarks
-     */
-    public StudentMarks()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+  public static void main(String[] args){
+      
+      int StudentCount = 30;
+      float[] marks = new float[StudentCount];
+      Scanner scan = new Scanner(System.in);
+      
+  }
 }
